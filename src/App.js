28f7775659a9e988292d7174b751e80127849a78
Darek Kilian <emis@MacBook-Pro.local>
2020-10-01 20:47:59 +0200

    Dodanie material UI oraz sidebaru z inputami

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import Map from "./components/Map/Map.js";
+// import Map from "./components/Map/Map.js";
+// import Layout from "./containers/Layout/Layout.js";
 import MapHooks from "./components/Map/MapHooks";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  mapContainer: {
+    position: "relative",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
+  const [lng, setLng] = useState(-104.9876);
+  const [lat, setLat] = useState(39.7405);
+  const [zoom, setZoom] = useState(12);
+
   return (
     <div className="App">
-      <MapHooks />
+      <Grid container spacing={3}>
+        <Grid className={classes.mapContainer} item xs={8}>
+          <MapHooks />
+        </Grid>
+        <Grid item xs={4}>
+          <div className={classes.paper}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="lng"
+              label="Szerokość geograficzna"
+              name="lng"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="lat"
+              label="Długość geograficzna"
+              name="lat"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="zoom"
+              label="Zoom"
+              name="zoom"
+              autoFocus
+            />
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Aux from "../Aux/Aux";
+import Grid from "@material-ui/core/Grid";
+const Layout = () => {
+  return <Aux>{this.props.children}</Aux>;
+};
+
+export default MapHooks;
