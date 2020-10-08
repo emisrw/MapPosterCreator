964f468dcf5d3f5a449163e9fa15822b6696086c
Darek Kilian <emis@MacBook-Pro.local>
2020-10-08 21:17:59 +0200

    Zmieniona nieco struktura, aktualizacja state przy przesuwaniu

--- src/App.js
+++ src/App.js
@@ -1,88 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-// import Map from "./components/Map/Map.js";
-// import Layout from "./containers/Layout/Layout.js";
-import MapHooks from "./components/Map/MapHooks";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  mapContainer: {
-    position: "relative",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-}));
+import Map from "./components/Map/Map";
 
 function App() {
-  const classes = useStyles();
-  const [lng, setLng] = useState(-104.9876);
-  const [lat, setLat] = useState(39.7405);
-  const [zoom, setZoom] = useState(12);
-
   return (
     <div className="App">
-      <Grid container spacing={3}>
-        <Grid className={classes.mapContainer} item xs={8}>
-          <MapHooks lng={lng} lat={lat} zoom={zoom} />
-        </Grid>
-        <Grid item xs={4}>
-          <div className={classes.paper}>
-            <form className={classes.form} noValidate>
-              <TextField
-                value={lng}
-                onChange={(e) => setLng(e.target.value)}
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                id="lng"
-                label="Szerokość geograficzna"
-                name="lng"
-                autoFocus
-              />
-              <TextField
-                value={lat}
-                onChange={(e) => setLat(e.target.value)}
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                id="lat"
-                label="Długość geograficzna"
-                name="lat"
-                autoFocus
-              />
-              <TextField
-                value={zoom}
-                onChange={(e) => setZoom(e.target.value)}
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                id="zoom"
-                label="Zoom"
-                name="zoom"
-                autoFocus
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                size="large"
-                color="primary"
-                className={classes.submit}
-              >
-                Aktualizuj
-              </Button>
-            </form>
-          </div>
-        </Grid>
-      </Grid>
+      <Map />
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,33 +1,130 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useRef, useEffect } from "react";
+import "./Map.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import mapboxgl from "mapbox-gl";
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZW1pc3J3IiwiYSI6ImNrZm83YmpvdTJmb2gyeG52MTB2cnM4OTgifQ.0ZH8kkQWf-2MXp_BGJo7HA";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //@50.0606372,19.9395669,14z
-      lng: 21.2680569,
-      lat: 50.196359,
-      zoom: 13,
-    };
-  }
-  componentDidMount() {
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const useStyles = makeStyles((theme) => ({
+  mapContainer: {
+    position: "relative",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
+
+function Map() {
+  const mapContainerRef = useRef(null);
+  const classes = useStyles();
+
+  const [zoom, setZoom] = useState(12);
+  const [lng, setLng] = useState(-104.9876);
+  const [lat, setLat] = useState(39.7405);
+
+  useEffect(() => {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: "mapbox://styles/emisrw/ckfobfyge018m19rujt5g5k0z",
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom,
+      center: [lng, lat],
+      zoom: zoom,
     });
-  }
 
-  render() {
-    return (
-      <div>
-        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
-      </div>
-    );
-  }
+    // add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+    // clean up on unmount
+    return () => map.remove();
+  }, [zoom]);
+
+  const handleChange = (event, newValue) => {
+    setZoom(newValue);
+  };
+  const handleSubmit = (event, newValue) => {
+    setZoom(newValue);
+  };
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <Grid container spacing={0}>
+        <Grid className={classes.mapContainer} item xs={8}>
+          <div className="map-container" ref={mapContainerRef} />
+        </Grid>
+        <Grid item xs={4}>
+          <div className={classes.paper}>
+            <form className={classes.form} onSubmit={handleSubmit} noValidate>
+              <TextField
+                value={lng}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="lng"
+                label="Szerokość geograficzna"
+                name="lng"
+                autoFocus
+              />
+              <TextField
+                value={lat}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="lat"
+                label="Długość geograficzna"
+                name="lat"
+                autoFocus
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                size="large"
+                color="primary"
+                className={classes.submit}
+              >
+                Aktualizuj
+              </Button>
+            </form>
+            <Box component="span" m={3}>
+              <Typography id="zoom-slider" gutterBottom>
+                Zoom level
+              </Typography>
+              <Slider
+                defaultValue={10}
+                // getAriaValueText={valuetext}
+                aria-labelledby="zoom-slider"
+                valueLabelDisplay="auto"
+                // onChange={(e) => setZoom(e.target.value)}
+                onChange={handleChange}
+                step={0.5}
+                marks
+                min={1}
+                max={12}
+              />
+            </Box>
+          </div>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
+
+export default Map;
